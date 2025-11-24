c51867231b3dd177797e123ba604fe380c9c61d3
DanielaVenturi <danielaventuri14@gmail.com>
2025-11-24 15:09:28 -0300

    NavBar simples Criada

--- listatarefa-front/src/components/Navbar.vue
+++ listatarefa-front/src/components/Navbar.vue
@@ -1,5 +1,8 @@
 <template>
-    <nav style="display: flex; gap: 20px; padding: 10px;background-color: blueviolet;">
-
-    </nav>
+  <nav style="display:flex; gap:20px; padding:10px; background:#eee;">
+    <router-link to="">Tarefas</router-link>
+    <router-link to="">Adicionar Tarefa</router-link>
+  </nav>
 </template>
+
+<script setup></script>
